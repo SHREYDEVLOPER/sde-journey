7e0e2cbcf14fefda5bf896434732098594538cf5
Shreyan Sharma <121509007+SHREYDEVLOPER@users.noreply.github.com>
2024-09-22 21:45:53 +0530

    Time: 531 ms (6.67%) | Memory: 306.2 MB (6.67%) - LeetSync

---
+++ 3541-report-spam-message/report-spam-message.cpp
@@ -0,0 +1,14 @@
+ class Solution {
+public:
+    bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
+        unordered_map<string,int>mp;
+        for(auto i:bannedWords)mp[i]++;
+        int cnt=0;
+        for(auto i:message){
+            if(mp.count(i))cnt++;
+            if(cnt>1)return 1;
+        }
+        return 0;
+    }
+
+};
