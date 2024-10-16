4378b8c4f2a4d16ae941a881212881a331fd7421
Shreyan Sharma <121509007+SHREYDEVLOPER@users.noreply.github.com>
2024-10-16 19:06:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (8.76%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.cpp
@@ -0,0 +1,15 @@
+ class Solution {
+public:
+    bool isThree(int n) {
+        int ct = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                ct++;
+                if (ct > 3) {
+                    return false;  
+                }
+            }
+        }
+        return ct == 3;  
+    }
+};
