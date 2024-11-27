20962d7d8b8b803045669fd5d39357c981a789e4
Shreyan Sharma <121509007+SHREYDEVLOPER@users.noreply.github.com>
2024-11-27 12:55:45 +0530

    Time: 1 ms (52.98%) | Memory: 10 MB (45.58%) - LeetSync

---
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        //hume kya karna hai special chaarcters of words
+        //matlab jo characters cpaoital form mai hai
+
+        //pehle duplicate ka chodd kahatm karunga 
+        sort(word.begin(),word.end());
+        set<char>uniq;
+        for(auto& i:word)
+        {
+            uniq.insert(i);
+        }
+
+        string mdw="";
+        for(auto& i:uniq)
+        {
+            mdw+=i;
+        }
+
+        //string ko alag alag karlo
+        string ss;
+        string bs;
+        for(auto& i:mdw)
+        {
+            if(i>='a'&&i<='z')
+            {
+                ss+=i;
+            }
+            else if(i>='A'&&i<='Z')
+            {
+                bs+=i;
+            }
+        }
+
+        map<char,int>bmp;
+        for(auto& i:bs)
+        {
+            bmp[i]++;
+        }
+        int ct=0;
+        for(auto& i:ss)
+        { 
+            char cb=i-'a'+'A';//CONVERSION IN CAPITAL
+            if(bmp.find(cb)!=bmp.end())
+            {
+                ct++;
+            }
+        }
+          return ct;
+    }
+};
