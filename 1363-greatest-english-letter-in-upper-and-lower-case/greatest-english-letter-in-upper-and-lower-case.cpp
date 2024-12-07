e8eab4d57f8a3e046f953bb4e547fce20692ecbc
Shreyan Sharma <121509007+SHREYDEVLOPER@users.noreply.github.com>
2024-12-07 16:08:58 +0530

    Time: 6 ms (21.68%) | Memory: 9.3 MB (38.91%) - LeetSync

---
+++ 1363-greatest-english-letter-in-upper-and-lower-case/greatest-english-letter-in-upper-and-lower-case.cpp
@@ -0,0 +1,29 @@
+ class Solution {
+public:
+    string greatestLetter(string s) {
+        vector<char> ans;  
+        unordered_map<char, int> mp;
+
+         for (auto& i : s) {
+            mp[i]++;
+        }
+
+         for (auto& i : mp) {
+            if (i.first >= 'A' && i.first <= 'Z') {  
+                char chk = tolower(i.first);  
+                if (mp.find(chk) != mp.end()) {  
+                    ans.push_back(i.first); 
+                }
+            }
+        }
+
+         sort(ans.begin(), ans.end());
+
+         if (ans.empty()) {
+            return ""; 
+        }
+
+         string result(1, ans.back());  
+        return result;
+    }
+};
