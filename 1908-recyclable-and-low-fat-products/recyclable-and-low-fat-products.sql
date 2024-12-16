319ef5da4e5bc9cd058aaad3645c665ed3e7da51
Shreyan Sharma <121509007+SHREYDEVLOPER@users.noreply.github.com>
2024-12-16 23:16:10 +0530

    Time: 1082 ms (5.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below##
+##pucha kya hai
+##id of the prodiuct that are low fat and recyclable
+##
+##y--recyclable
+##
+
+select product_id from Products where low_fats='Y'  AND recyclable='Y';
