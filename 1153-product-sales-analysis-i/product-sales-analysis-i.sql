2fc2273d437d753394ce486935abad3aa71754e7
Shreyan Sharma <121509007+SHREYDEVLOPER@users.noreply.github.com>
2024-12-17 21:39:27 +0530

    Time: 1656 ms (20.52%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+##Write a solution to report the product_name, year,
+## and price for each sale_id in the Sales table.
+
+##isme mai left join lagega
+SELECT sales.year, sales.price, product.product_name 
+FROM sales 
+LEFT JOIN product 
+ON sales.product_id = product.product_id;
