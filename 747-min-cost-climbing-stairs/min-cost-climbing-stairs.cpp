59353d0c35ddd009d9eb4abcf6728faf7aafbcdb
Shreyan Sharma <121509007+SHREYDEVLOPER@users.noreply.github.com>
2025-01-01 22:45:55 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (9.40%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
-    
-    int temp(int ind,vector<int>&c,vector<int>&dp)
-    {   
-        if(ind>=c.size())
+    int solve(vector<int>&cost,int n,vector<int>&dp)
+    {
+        if(n==0)
         {
-            return 0;
+            return cost[0];
         }
-        if(dp[ind]!=-1)
+        if(n==1)
         {
-            return dp[ind];
+            return cost[1];
         }
-        int op1=c[ind]+temp(ind+1,c,dp);
-        int op2=c[ind]+temp(ind+2,c,dp);
-        return dp[ind]=min(op1,op2);
-
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        dp[n]=cost[n]+min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+        return dp[n];
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        //output
-        //array hai
-        //cost[i]=cost of ith
-        //costv payed climb one or two
-        //start from s 0 or s 1
-        //min cost to reach top of the floor
-        //array end with min cost
-
-        // [10,20,30,40,50]
-        //sum of minimu, cost to bee return
-        vector<int>dp(cost.size()+1,-1);
-         
-        int a=temp(0,cost,dp);
-        int b=temp(1,cost,dp);
-        return min(a,b);
-
-
-     }
+        int n=cost.size();
+        vector<int>dp(n+1,-1);
+        int ans=min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+        return ans;
+        
+    }
 };
