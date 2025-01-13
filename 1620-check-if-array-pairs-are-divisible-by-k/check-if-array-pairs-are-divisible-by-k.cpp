8cc587ea5496b610a379396badcd6fe3bd262aab
Shreyan Sharma <121509007+SHREYDEVLOPER@users.noreply.github.com>
2025-01-13 13:34:17 +0530

    Time: 47 ms (40.38%) | Memory: 83.4 MB (32.67%) - LeetSync

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,24 @@
+ class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        unordered_map<int, int> remainderCount;
+
+         for (int num : arr) {
+            int rem = ((num % k) + k) % k;   
+            remainderCount[rem]++;
+        }
+
+         for (auto& [rem, count] : remainderCount) {
+            if (rem == 0) {
+                 if (count % 2 != 0) return false;
+            } else {
+                 int complement = k - rem;
+                if (remainderCount[complement] != count) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
