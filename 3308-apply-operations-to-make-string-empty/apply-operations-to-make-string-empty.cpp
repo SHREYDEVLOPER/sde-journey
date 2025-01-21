6b7532b92ced5ef14e4b9c9d763cfcfb98ad683d
Shreyan Sharma <121509007+SHREYDEVLOPER@users.noreply.github.com>
2025-01-21 22:28:18 +0530

    Time: 110 ms (46.19%) | Memory: 32.5 MB (62.50%) - LeetSync

---
+++ 3308-apply-operations-to-make-string-empty/apply-operations-to-make-string-empty.cpp
@@ -0,0 +1,22 @@
+ class Solution {
+public:
+    string lastNonEmptyString(string s) {
+         unordered_map<char,int> ump; 
+        int maxi=0; 
+        for(auto it:s){
+            ump[it]++;
+             maxi=max(ump[it],maxi);
+        }
+         string ans="";
+         for(int i=s.size()-1;i>=0;i--){
+            if(ump[s[i]]==maxi){
+               
+                ans.push_back(s[i]);
+                
+                ump[s[i]]--;
+            }
+        }
+         reverse(ans.begin(),ans.end());
+          return ans;
+    }
+};
