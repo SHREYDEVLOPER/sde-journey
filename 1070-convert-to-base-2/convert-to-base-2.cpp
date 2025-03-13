a6f307499c9f5b4f371924d288f88c95ee39b91b
Shreyan Sharma <121509007+SHREYDEVLOPER@users.noreply.github.com>
2025-03-13 12:46:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (23.20%) - LeetSync

---
+++ 1070-convert-to-base-2/convert-to-base-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string baseNeg2(int n) {
+        if (n == 0) return "0";
+      string result = "";
+    while (n != 0) {
+        int remainder= n % -2;
+        n = n / -2;
+        if (remainder< 0) {
+            remainder += 2;
+            n++;
+        }
+        result = to_string(remainder)+ result;
+    }
+    return result;
+
+    }
+};
